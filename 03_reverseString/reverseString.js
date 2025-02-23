f07072803cc9a6fa1d690bc4e0861db1a5dab536
Rendi Doran <[rendirdoran@gmail.com]>
2025-02-23 14:59:06 -0600

    Add a console.log to the end of the code.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -2,7 +2,7 @@ function reverseString(str) {
 return str.split('').reverse().join('');
 };
 
-console.log(reverseString("Hello There!"));
+console.log(reverseString());
 
 // Do not edit below this line
 export default reverseString;
